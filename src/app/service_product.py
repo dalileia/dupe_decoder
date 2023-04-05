b3155272bdb46ee683975c8914883c0640f162ba
dalileia <dalileia>
2023-04-05 13:27:09 -0300

    simplify objects serialization

--- src/app/service_product.py
+++ src/app/service_product.py
@@ -3,7 +3,6 @@ from flask_restful import Resource
 import json
 from app.dao.dao import Dao
 from app.model.product import Product
-from app.helper.json_object_helper import Object
 from app.helper.product_helper import ProductHelper
 from app.model.compared_product_list import ComparedProductList
 
@@ -12,11 +11,11 @@ class GetAllProducts(Resource):
     def get(self):
         products = None
         try:
-            result_to_json = Object()
-            result_to_json.products = Dao().return_all_products()
-            result = result_to_json.toJSON()
-            products = json.loads(result)
-        except:
+            result = Dao().return_all_products()
+            products = json.dumps(result, default=lambda o: o.__dict__, sort_keys=True)
+            products = json.loads(products)
+        except Exception as e:
+            print(e)
             products = None
         return products
 
@@ -25,17 +24,15 @@ class GetOneProduct(Resource):
     def get(self, id):
         product = None
         product_id_list = ProductHelper.product_id_list()
-        
         if (id not in product_id_list):
             return make_response(jsonify({"error": "400"}), 400)
         
         else:
             try:
-                result_to_json = Object()
-                result_to_json.product = Dao().return_one_product(id)
-                result = result_to_json.toJSON()
-                product = json.loads(result)
-            except:
+                product_data = Dao().return_one_product(id)
+                product = json.dumps(product_data, default=lambda o: o.__dict__)
+            except Exception as e:
+                print(e)
                 product = None
             return product
     
@@ -44,7 +41,6 @@ class CompareProducts(Resource):
     def get(self, id):
         compared_products = None
         product_id_list = ProductHelper.product_id_list()
-        
         if (id not in product_id_list):
             return make_response(jsonify({"error": "400"}), 400)
         
@@ -56,11 +52,9 @@ class CompareProducts(Resource):
                 product.hydrate(product_data)
                 result = ProductHelper.compared_product_list(product,product_list)
                 result.sort(key=lambda x: x.percentage, reverse=True)
-                result_to_json = Object()
-                result_to_json.compared_product_list = ComparedProductList(product,result)
-                result = result_to_json.toJSON()
-                compared_products = json.loads(result)
-            except:
+                compared_products = json.dumps(result, default=lambda o: o.__dict__)
+            except Exception as e:
+                print(e)
                 compared_products = None
             return compared_products
     
@@ -73,10 +67,7 @@ class SearchProducts(Resource):
             for product in product_list:
                 if(product.name.lower().find(searched) != -1): 
                     result.append(product)
-            result_to_json = Object()
-            result_to_json.products = result
-            result = result_to_json.toJSON()
-            searched_products = json.loads(result)          
+            searched_products = json.dumps(result, default=lambda o: o.__dict__)
         except:
             searched_products = None
         return searched_products
