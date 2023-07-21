5490975be934cb17e2d2ef5f14e4f907384740fa
dalileia <dalileia>
2023-07-21 11:22:40 -0300

    corrige end migrations

--- backend/product/start.sh
+++ backend/product/start.sh
@@ -2,13 +2,13 @@
 
 if [ "$ENV_PRODUCTION" = "yes" ] ; then
     sleep 15s
-    yoyo init --database mysql://root:@mysql/product migrations
+    yoyo init --database mysql://$ENV_MYSQL_USER:$ENV_MYSQL_PASSWORD@$ENV_MYSQL_HOST/product migrations
     yoyo apply
     uwsgi --ini python.ini
 else
     export FLASK_DEBUG=1
     sleep 15s
-    yoyo init --database mysql://root:@mysql/product migrations
+    yoyo init --database mysql://$ENV_MYSQL_USER:$ENV_MYSQL_PASSWORD@$ENV_MYSQL_HOST/product migrations
     yoyo apply
     flask --app app run --host 0.0.0.0 --port 5000
 fi
